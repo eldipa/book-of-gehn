463f166904083341af30d93eec62464c49ddebbb
Martin Di Paola <martinp.dipaola@gmail.com>
2020-02-14 13:08:32 +0000

    Extend plugin to support marginnotes of code (markdown)

--- _plugins/marginnote.rb
+++ _plugins/marginnote.rb
@@ -1,4 +1,17 @@
-## Usage {% marginnote 'Your text here' %}
+## Usage (for simple texts)
+#   {% marginnote 'Your text here' %}
+#
+## Usage (for code and text)
+#   {% marginmarkdowncode
+#   '
+#   ```cpp
+#   while (1)
+#      more code
+#   ```
+#   '
+#
+#   'text here'
+#   %}
 module Jekyll
   class RenderMarginNoteTag < Liquid::Tag
 
@@ -20,5 +33,39 @@ module Jekyll
   end
 end
 
+module Jekyll
+  class RenderMarginMarkdownCodeTag < Liquid::Tag
+
+    require "shellwords"
+    require "digest"
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text.shellsplit
+    end
+
+    def render(context)
+      comments = @text[1] || ""
+
+      token = Digest::MD5.hexdigest "#{@text[0] + comments}"
+      id = "mmkd-#{token}"
+
+      site = context.registers[:site]
+      markdown = Converters::Markdown.new(site.config)
+      markdown.setup
+      payload = markdown.convert("#{@text[0]}").strip
+
+      # escape any single quote
+      #payload = payload.gsub(/\\([\s\S])|(')/,"\\$1$2");
+
+      "<label for='#{id}' class='margin-toggle'> &#8853;</label>"+
+      "<input type='checkbox' id='#{id}' class='margin-toggle'/>"+
+      "\n<div id='mk-#{id}'><span class='marginnote marginmarkdowncode'>#{payload}#{comments}</span></div>\n"+
+      "<div><script>$(document).ready(function () {$('#mk-#{id} > span').insertAfter($('##{id}'))});</script></div>"
+    end
+  end
+end
+
 Liquid::Template.register_tag('marginnote', Jekyll::RenderMarginNoteTag)
+Liquid::Template.register_tag('marginmarkdowncode', Jekyll::RenderMarginMarkdownCodeTag)
 
